287c9e25605766f60962f7a8d360d4a8f31b8ca8
Ben Venker <bvemails@gmail.com>
2020-03-06 08:26:59 -0800

    create hooks for score states

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@
 import React from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
+import { useState } from "react";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  const [homeScore, setHomeScore] = useState(0);
+  const [awayScore, setAwayScore] = useState(0);
 
   return (
     <div className="container">
